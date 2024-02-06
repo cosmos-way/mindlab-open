ce5ec5c17d5313f0f59d1fde12e201507164f1fd
cosmos <kostyakiselev@gmail.com>
2024-02-06 20:26:22 +0300

    добавил сохранение в CSV

--- raw_repository/processing.py
+++ raw_repository/processing.py
@@ -1,5 +1,6 @@
 import os
 import re
+import csv
 from . import gonogo
 
 # Компилируем регулярные выражения для черного и белого списков
@@ -35,18 +36,21 @@ def process_files_in_directory(directory, handler):
         if os.path.isfile(file_path) and whitelist_regex.search(file):
             handler(file_path)
 
-def process_directory(root_directory):
-    """Целенаправленный обход каталогов для обработки файлов."""
-    for respondent_id in os.listdir(root_directory):
-        respondent_path = os.path.join(root_directory, respondent_id)
-        if not os.path.isdir(respondent_path):
-            continue  # Пропускаем файлы на уровне root_directory
-        for day in ['1day', '2day', 'day1', 'day2']:
-            day_path = os.path.join(respondent_path, day)
-            if not os.path.isdir(day_path):
-                continue  # Пропускаем, если директория дня отсутствует
-            for content_type in content_handlers.keys():
-                content_path = os.path.join(day_path, content_type)
-                if os.path.isdir(content_path):
-                    # Здесь можно добавить многопоточность или асинхронную обработку
-                    process_files_in_directory(content_path, content_handlers[content_type])
+def process_directory(root_directory, output_csv_path):
+    with open(output_csv_path, 'w', newline='', encoding='utf-8') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=['respondentID', 'day', 'times', 'response_time', 'accuracy', 'overall_score', 'attempt_num', 'score'])
+        csv_writer.writeheader()  # Запись заголовка
+        for respondent_id in os.listdir(root_directory):
+            respondent_path = os.path.join(root_directory, respondent_id)
+            if not os.path.isdir(respondent_path):
+                continue  # Пропускаем файлы на уровне root_directory
+            for day in ['1day', '2day', 'day1', 'day2']:
+                day_path = os.path.join(respondent_path, day)
+                if not os.path.isdir(day_path):
+                    continue  # Пропускаем, если директория дня отсутствует
+                for content_type in content_handlers.keys():
+                    content_path = os.path.join(day_path, content_type)
+                    if os.path.isdir(content_path):
+                        # Здесь можно добавить многопоточность или асинхронную обработку
+                        data = process_files_in_directory(content_path, content_handlers[content_type])
+                        csv_writer.writerow(data)
